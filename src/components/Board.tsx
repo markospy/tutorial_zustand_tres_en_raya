7ccd06fbbdea8c5621fda2ef043dc9cd572be980
Marcos Avila <marcos.seocuba@gmail.com>
2024-12-22 08:46:46 -0500

    First commit

---
+++ src/components/Board.tsx
@@ -0,0 +1,42 @@
+import { Square } from "./Square";
+import { calculateWinner, calculateTurns, calculateStatus } from "../utils/utils"
+
+
+export default function Board({ xIsNext, squares, onPlay }) {
+  const winner = calculateWinner(squares)
+  const turns = calculateTurns(squares)
+  const player = xIsNext ? 'X' : 'O'
+  const status = calculateStatus(winner, turns, player)
+
+  function handleClick(i) {
+    if (squares[i] || winner) return
+    const nextSquares = squares.slice()
+    nextSquares[i] = player
+    onPlay(nextSquares)
+  }
+
+  return (
+    <>
+      <div style={{ marginBottom: '0.5rem' }}>{status}</div>
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(3, 1fr)',
+          gridTemplateRows: 'repeat(3, 1fr)',
+          width: 'calc(3 * 2.5rem)',
+          height: 'calc(3 * 2.5rem)',
+          border: '1px solid #999',
+        }}
+      >
+        {squares.map((square, squareIndex) => (
+          <Square
+            key={squareIndex}
+            value={square}
+            onSquareClick={() => handleClick(squareIndex)}
+          />
+        ))}
+      </div>
+    </>
+  )
+}
+
---
+++ src/components/Game.tsx
@@ -0,0 +1,79 @@
+import { create } from "zustand";
+import { combine } from "zustand/middleware";
+import Board from "./Board";
+
+const useGameStore = create(
+  combine(
+    { history: [Array(9).fill(null)], currentMove: 0 },
+    (set) => {
+      return {
+        setHistory: (nextHistory) => {
+          set((state) => ({
+            history:
+              typeof nextHistory === 'function'
+                ? nextHistory(state.history)
+                : nextHistory,
+          }))
+        },
+        setCurrentMove: (nextCurrentMove) => {
+          set((state) => ({
+            currentMove:
+              typeof nextCurrentMove === 'function'
+                ? nextCurrentMove(state.currentMove)
+                : nextCurrentMove,
+          }))
+        },
+      }
+    },
+  ),
+)
+
+
+export default function Game() {
+  const { history, setHistory, currentMove, setCurrentMove } = useGameStore()
+  const xIsNext = currentMove % 2 === 0
+  const currentSquares = history[currentMove]
+
+
+  function handlePlay(nextSquares) {
+    const nextHistory = history.slice(0, currentMove + 1).concat([nextSquares])
+    setHistory(nextHistory)
+    setCurrentMove(nextHistory.length - 1)
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove)
+  }
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+        fontFamily: 'monospace',
+      }}
+    >
+      <div>
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div style={{ marginLeft: '1rem' }}>
+        <ol>
+          {history.map((_, historyIndex) => {
+            const description =
+              historyIndex > 0
+                ? `Go to move #${historyIndex}`
+                : 'Go to game start'
+
+            return (
+              <li key={historyIndex}>
+                <button onClick={() => jumpTo(historyIndex)}>
+                  {description}
+                </button>
+              </li>
+            )
+          })}
+        </ol>
+      </div>
+    </div>
+  )
+}
